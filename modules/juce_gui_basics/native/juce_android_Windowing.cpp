190f4b2c582879afa30505efeb2c99058ef9dab8
reuk <reuk@users.noreply.github.com>
2022-05-19 16:22:22 +0100

    TextEditor:  Add option to dismiss the virtual keyboard on touches outside
    Previously, individual components had to ask the peer to hide and show
    the keyboard, by calling textInputRequired() and
    dismissPendingTextInput() respectively. When an onscreen keyboard (OSK)
    was required, most Peer implementation would directly hide/show the OSK
    inside these function. However, the iOS ComponentPeer implementation
    instead listened to the application's global keyboard focus, and only
    opened the OSK when the focused component was also a TextInputTarget
    with active input.
    
    The iOS scheme seems like a better design, as it enforces that the OSK
    hiding and showing is synced with the keyboard focus of the application.
    In the other implementations, it was possible for a Component to call
    textInputRequired even when it didn't have the keyboard focus, putting
    the application into an inconsistent state. The iOS scheme also makes
    the TextInputTarget interface more useful, as it enforces that the OSK
    will only display for components that implement TextInputTarget, and
    return true from isTextInputActive().
    
    This patch changes all Peer implementations to match the iOS
    implementation, improving consistency. Each time the global keyboard
    focus changes, refreshTextInputTarget is called automatically, and the
    OSK is shown if the focused component is a TextInputTarget that returns
    true from isTextInputActive, and hidden otherwise. Components can also
    call refreshTextInputTarget manually. This should be done whenever the
    component updates the return value of isTextInputActive(). Effectively,
    the Peer is now responsible for keeping track of the focused
    TextInputTarget, rather than allowing individual components to hide and
    show the OSK at will.
    
    Additionally, this patch adds an option to the TextEditor to
    automatically dismiss the OSK when the mouse is clicked outside of the
    editor. This should improve user experience on mobile platforms, where
    touches on sibling components may cause a TextEditor to gain keyboard
    focus and unnecessarily display the OSK.

--- modules/juce_gui_basics/native/juce_android_Windowing.cpp
+++ modules/juce_gui_basics/native/juce_android_Windowing.cpp
@@ -1186,6 +1186,8 @@ public:
 
     void dismissPendingTextInput() override
     {
+        closeInputMethodContext();
+
         view.callVoidMethod (ComponentPeerView.showKeyboard, javaString ("").get());
 
         if (! isTimerRunning())
