de9fc6c76ac73d8a9bb06b5deeccb3311cbdb3a4
Matthieu <matthieu.rotschi@orosys.fr>
2023-02-06 10:33:59 +0100

    Change BT permissions for API >31

--- modules/juce_core/native/juce_android_RuntimePermissions.cpp
+++ modules/juce_core/native/juce_android_RuntimePermissions.cpp
@@ -29,7 +29,7 @@ static String jucePermissionToAndroidPermission (RuntimePermissions::PermissionI
     switch (permission)
     {
         case RuntimePermissions::recordAudio:           return "android.permission.RECORD_AUDIO";
-        case RuntimePermissions::bluetoothMidi:         return "android.permission.ACCESS_FINE_LOCATION";
+        case RuntimePermissions::bluetoothMidi:         return "android.permission.BLUETOOTH_CONNECT";
         case RuntimePermissions::readExternalStorage:   return "android.permission.READ_EXTERNAL_STORAGE";
         case RuntimePermissions::writeExternalStorage:  return "android.permission.WRITE_EXTERNAL_STORAGE";
         case RuntimePermissions::camera:                return "android.permission.CAMERA";
@@ -43,7 +43,7 @@ static String jucePermissionToAndroidPermission (RuntimePermissions::PermissionI
 static RuntimePermissions::PermissionID androidPermissionToJucePermission (const String& permission)
 {
     if      (permission == "android.permission.RECORD_AUDIO")            return RuntimePermissions::recordAudio;
-    else if (permission == "android.permission.ACCESS_FINE_LOCATION")    return RuntimePermissions::bluetoothMidi;
+    else if (permission == "android.permission.BLUETOOTH_CONNECT")    return RuntimePermissions::bluetoothMidi;
     else if (permission == "android.permission.READ_EXTERNAL_STORAGE")   return RuntimePermissions::readExternalStorage;
     else if (permission == "android.permission.WRITE_EXTERNAL_STORAGE")  return RuntimePermissions::writeExternalStorage;
     else if (permission == "android.permission.CAMERA")                  return RuntimePermissions::camera;
